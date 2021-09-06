ee97f7de5e1e94ccdb525a1f83ee8833ede8332d
dmytroyevlakhov <yevlakhovdmytro@gmail.com>
2021-09-06 22:13:44 +0300

    removed comments

--- handlers.go
+++ handlers.go
@@ -6,12 +6,10 @@ import (
 	"github.com/takama/router"
 )
 
-// home returns the path of current request
 func home(c *router.Control) {
 	fmt.Fprintf(c.Writer, "Processing URL %s...", c.Request.URL.Path)
 }
 
-// logger provides a log of requests
 func logger(c *router.Control) {
 	remoteAddr := c.Request.Header.Get("X-Forwarded-For")
 	if remoteAddr == "" {
